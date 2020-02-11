094f2116c4f1acb09719a16e1287909ca12296ca
u17855156 <bulaev.a.u@gmail.com>
2020-02-12 01:31:01 +0300

    #2.2 - finishes

---
+++ functionalInterfacesInJavaBook/src/main/java/chapter2/problem2/SummerLambda.java
@@ -0,0 +1,6 @@
+package chapter2.problem2;
+
+@FunctionalInterface
+public interface SummerLambda<T> {
+    T compute(T t);
+}
---
+++ functionalInterfacesInJavaBook/src/main/java/chapter2/problem2/SummerLambdaImplementation.java
@@ -0,0 +1,7 @@
+package chapter2.problem2;
+
+public class SummerLambdaImplementation {
+    SummerLambda<Integer> intSum = x -> x+x;
+    SummerLambda<Double> doubleSum = x -> x+x;
+    SummerLambda<Long> longSum = x -> x+x;
+}
