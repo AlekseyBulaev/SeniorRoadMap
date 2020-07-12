9522de2dd3485eba360f4c16b839a77ee32d95d0
Alexey <assass1n@bk.ru>
2020-07-12 14:17:19 +0300

    leetCode. LongestSubstringWithoutRepeatingCharacters

---
+++ root/leetcode/src/main/java/medium/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,16 @@
+package medium;
+
+//Given a string, find the length of the longest substring without repeating characters.
+public class LongestSubstringWithoutRepeatingCharacters {
+    public static int lengthOfLongestSubstring(String s) {
+        int n = s.length(), ans = 0;
+        int[] index = new int[128]; // current index of character
+        // try to extend the range [i, j]
+        for (int j = 0, i = 0; j < n; j++) {
+            i = Math.max(index[s.charAt(j)], i);
+            ans = Math.max(ans, j - i + 1);
+            index[s.charAt(j)] = j + 1;
+        }
+        return ans;
+    }
+}
